c968ec465c906d26a18a78c848d7d1d88202b9b3
Marcelo <marcelo073020@hotmail.com>
2021-12-06 22:21:09 -0500

    Inicio de Proyecto

---
+++ ATMWSSOAP/Dominio/Cliente.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ATMWSSOAP.Dominio
+{
+    public class Cliente
+    {
+    }
+}
---
+++ ATMWSSOAP/IATMService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ATMWSSOAP
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IATMService" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IATMService
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
