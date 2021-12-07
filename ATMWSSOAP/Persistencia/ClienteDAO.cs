9462573a727ac630e0943eb654add153e3d7b4fe
Marcelo <marcelo073020@hotmail.com>
2021-12-06 23:11:41 -0500

    DAO

--- ATMWSSOAP/Persistencia/ClienteDAO.cs
+++ ATMWSSOAP/Persistencia/ClienteDAO.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ATMWSSOAP.Dominio;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,30 @@ namespace ATMWSSOAP.Persistencia
 {
     public class ClienteDAO
     {
+        private string CadenaConexion = "Data Source=(local); Initial Catalog=ATMDB;Integrated Security=SSPI;";
+
+        public Cliente Crear(Cliente clienteACrear)
+        {
+            Cliente clienteCreado;
+            string sql = "INSERT INTO location_owners VALUES(@name,@business_name,@ruc)";
+            using (SqlConnection conexion = new SqlConnection(CadenaConexion))
+            {
+                conexion.Open();
+                using (SqlCommand comando = new SqlCommand(sql, conexion))
+                {
+                    comando.Parameters.Add(new SqlParameter("@name", clienteACrear.Nombre));
+                    comando.Parameters.Add(new SqlParameter("@business_name", clienteACrear.Empresa));
+                    comando.Parameters.Add(new SqlParameter("@ruc", clienteACrear.Ruc));
+                    comando.ExecuteNonQuery();
+                }
+                clienteCreado = Obtener(clienteACrear.Nombre);
+                return clienteCreado;
+            }
+        }
+
+        public Cliente Obtener(string ruc)
+        {
+
+        }
     }
 }
