35aa0d29452859e4b115c17a0ccf8860d6f301b7
Alonso <alonso_vento@outlook.com>
2021-12-08 19:01:28 -0500

    últimos cambios

---
+++ BD ATMs/eliminar datos.sql
@@ -0,0 +1,28 @@
+use ATMDB
+go
+
+delete from  [dbo].[atms] 
+DBCC CHECKIDENT ('atms', RESEED,0)
+delete from  [dbo].[atm_types] 
+DBCC CHECKIDENT ('atm_types', RESEED,0)
+delete  from  [dbo].[atm_models] 
+DBCC CHECKIDENT ('atm_models', RESEED,0)
+delete from  [dbo].[atm_states] 
+DBCC CHECKIDENT ('atm_states', RESEED,0)
+delete from  [dbo].[atm_brands] 
+DBCC CHECKIDENT ('atm_brands', RESEED,0)
+
+delete from [locations]
+DBCC CHECKIDENT ('locations', RESEED,0)
+delete from location_owners
+DBCC CHECKIDENT ('location_owners', RESEED,0)
+delete from location_types
+DBCC CHECKIDENT ('location_types', RESEED,0)
+
+delete from districts
+DBCC CHECKIDENT ('districts', RESEED,0)
+delete from provinces
+DBCC CHECKIDENT ('provinces', RESEED,0)
+delete from departments
+DBCC CHECKIDENT ('departments', RESEED,0)
+
---
+++ SistemaATM/Controllers/ATMsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaATM.Controllers
+{
+    public class ATMsController : Controller
+    {
+        // GET: ATMs
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
